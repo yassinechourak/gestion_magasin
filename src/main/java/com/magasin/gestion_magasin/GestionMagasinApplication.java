6984a43c7cb095b1d122f2e543dc9f5a43a5ed12
yassin <yassin@yhost.168.1.1>
2026-05-17 18:11:05 +0100

    first commit

---
+++ src/main/java/com/magasin/gestion_magasin/GestionMagasinApplication.java
@@ -0,0 +1,13 @@
+package com.magasin.gestion_magasin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMagasinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMagasinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magasin/gestion_magasin/GestionMagasinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magasin.gestion_magasin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMagasinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
